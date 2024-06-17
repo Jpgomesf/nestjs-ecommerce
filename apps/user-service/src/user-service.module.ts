af94320b0f4c6ca268a7c6cbf17ec6bb6f7cbfd9
Joao Pedro Gomes Ferreira <jpgomesf@Joaos-MacBook-Pro.local>
2024-06-16 22:43:25 -0300

    :sparkles: feat: Update user module with typeORM.

--- apps/user-service/src/user-service.module.ts
+++ apps/user-service/src/user-service.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { UserServiceController } from './user-service.controller';
-import { UserServiceService } from './user-service.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user-service.controller';
+import { UserService } from './user-service.service';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
-  controllers: [UserServiceController],
-  providers: [UserServiceService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'mydb',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
 })
-export class UserServiceModule {}
+export class UserServiceModule { }
