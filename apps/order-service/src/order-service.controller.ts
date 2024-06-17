5a2aab6ea498387eb873cafe9b46b6b72665bf2c
Joao Pedro Gomes Ferreira <jpgomesf@Joaos-MacBook-Pro.local>
2024-06-17 01:08:41 -0300

    :sparkles: feat: Create simple CRUD for orders.

--- apps/order-service/src/order-service.controller.ts
+++ apps/order-service/src/order-service.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
-import { OrderServiceService } from './order-service.service';
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { OrderService } from './order-service.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Controller()
-export class OrderServiceController {
-  constructor(private readonly orderServiceService: OrderServiceService) {}
+export class OrderController {
+  constructor(private readonly orderService: OrderService) { }
 
-  @Get()
-  getHello(): string {
-    return this.orderServiceService.getHello();
+  @MessagePattern('get-orders')
+  findAll() {
+    return this.orderService.findAll();
+  }
+
+  @MessagePattern('get-order')
+  findOne(id: number) {
+    return this.orderService.findOne(id);
+  }
+
+  @MessagePattern('create-order')
+  create(createOrderDto: CreateOrderDto) {
+    return this.orderService.create(createOrderDto);
+  }
+
+  @MessagePattern('update-order')
+  update({ id, updateOrderDto }: { id: number; updateOrderDto: UpdateOrderDto }) {
+    return this.orderService.update(id, updateOrderDto);
+  }
+
+  @MessagePattern('delete-order')
+  remove(id: number) {
+    return this.orderService.remove(id);
   }
 }
--- apps/order-service/src/order-service.service.ts
+++ apps/order-service/src/order-service.service.ts
@@ -1,8 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from './order.entity';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { ClientKafka } from '@nestjs/microservices';
+import { OrderCreatedEvent } from './events/order-created.envent';
+import { OrderPaidEvent } from './events/order-paid.event';
 
 @Injectable()
-export class OrderServiceService {
-  getHello(): string {
-    return 'Hello World!';
+export class OrderService {
+  constructor(
+    @InjectRepository(Order)
+    private orderRepository: Repository<Order>,
+    @Inject('KAFKA_CLIENT') private readonly kafkaClient: ClientKafka,
+  ) { }
+
+  async create(createOrderDto: CreateOrderDto): Promise<Order> {
+    const order = this.orderRepository.create({
+      ...createOrderDto,
+      status: 'CREATED',
+    });
+    const savedOrder = await this.orderRepository.save(order);
+
+    this.kafkaClient.emit(
+      'order-events',
+      new OrderCreatedEvent(
+        savedOrder.id.toString(),
+        savedOrder.userId,
+        savedOrder.items,
+        savedOrder.totalPrice,
+      ),
+    );
+
+    return savedOrder;
+  }
+
+  findAll(): Promise<Order[]> {
+    return this.orderRepository.find();
+  }
+
+  findOne(id: number): Promise<Order> {
+    return this.orderRepository.findOne({ where: { id: id } });
+  }
+
+  async update(id: number, updateOrderDto: UpdateOrderDto): Promise<Order> {
+    await this.orderRepository.update(id, updateOrderDto);
+    const updatedOrder = await this.orderRepository.findOne({ where: { id: id } });
+
+    if (updatedOrder.status === 'PAID') {
+      this.kafkaClient.emit(
+        'order-events',
+        new OrderPaidEvent(updatedOrder.id.toString(), updatedOrder.status),
+      );
+    }
+
+    return updatedOrder;
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.orderRepository.delete(id);
   }
 }
