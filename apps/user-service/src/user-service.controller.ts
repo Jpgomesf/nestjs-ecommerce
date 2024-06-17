278f0fb7a7b3d5f3206ad3a980545dae2444befe
Joao Pedro Gomes Ferreira <jpgomesf@Joaos-MacBook-Pro.local>
2024-06-16 22:43:55 -0300

    :sparkles: feat: Create simple CRUD for users.

--- apps/user-service/src/user-service.controller.ts
+++ apps/user-service/src/user-service.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
-import { UserServiceService } from './user-service.service';
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { UserService } from './user-service.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller()
-export class UserServiceController {
-  constructor(private readonly userServiceService: UserServiceService) {}
+export class UserController {
+  constructor(private readonly userService: UserService) { }
 
-  @Get()
-  getHello(): string {
-    return this.userServiceService.getHello();
+  @MessagePattern('get-users')
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @MessagePattern('get-user')
+  findOne(id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @MessagePattern('create-user')
+  create(createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @MessagePattern('update-user')
+  update({ id, updateUserDto }: { id: number; updateUserDto: UpdateUserDto }) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @MessagePattern('delete-user')
+  remove(id: number) {
+    return this.userService.remove(id);
   }
 }
+
--- apps/user-service/src/user-service.service.ts
+++ apps/user-service/src/user-service.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UserServiceService {
-  getHello(): string {
-    return 'Hello World!';
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) { }
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number): Promise<User> {
+    return this.userRepository.findOneOrFail({ where: { id: id } });
+  }
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    await this.userRepository.update(id, updateUserDto);
+    return this.userRepository.findOneOrFail(({ where: { id: id } }));
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.userRepository.delete(id);
   }
 }
+
