7e51950f8d2b8d1a3110a2678939d469f9393a49
Joao Pedro Gomes Ferreira <jpgomesf@Joaos-MacBook-Pro.local>
2024-06-17 01:08:17 -0300

    :sparkles: feat: Create producer and events
    Create an event for creation and one to register the payment.

--- apps/order-service/src/order-service.module.ts
+++ apps/order-service/src/order-service.module.ts
@@ -1,10 +1,40 @@
+import { OrderController } from './order-service.controller';
+import { OrderService } from './order-service.service';
 import { Module } from '@nestjs/common';
-import { OrderServiceController } from './order-service.controller';
-import { OrderServiceService } from './order-service.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { Order } from './order.entity';
 
 @Module({
-  imports: [],
-  controllers: [OrderServiceController],
-  providers: [OrderServiceService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'mydb',
+      entities: [Order],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Order]),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_CLIENT',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['kafka:9092'],
+          },
+          consumer: {
+            groupId: 'order-service-consumer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [OrderController],
+  providers: [OrderService],
 })
-export class OrderServiceModule {}
+export class OrderServiceModule { }
+
